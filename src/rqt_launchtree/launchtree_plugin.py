46ae778ffb3819d7db5bc56fbcb59d859c9cfe9c
Philipp Schillinger <philsplus@gmail.com>
2016-12-11 17:52:27 +0100

    Rebase from 'debian/indigo/rqt_launchtree'

--- src/rqt_launchtree/launchtree_plugin.py
+++ src/rqt_launchtree/launchtree_plugin.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import rospy
 from rqt_gui_py.plugin import Plugin
 
 from python_qt_binding.QtCore import Qt
@@ -8,6 +9,10 @@ from rqt_launchtree.launchtree_widget import LaunchtreeWidget
 
 
 class LaunchtreePlugin(Plugin):
+
+    _SETTING_LASTPKG = 'last_pkg'
+    _SETTING_LASTLAUNCHFILE = 'last_launch'
+    _SETTING_LASTLAUNCHARGS = 'last_args'
 
     def __init__(self, context):
         super(LaunchtreePlugin, self).__init__(context)
@@ -23,9 +28,29 @@ class LaunchtreePlugin(Plugin):
 
     def save_settings(self, plugin_settings, instance_settings):
         instance_settings.set_value('editor', self._widget.editor)
+        _curr_pkg = self._widget.package_select.currentText()
+        rospy.logdebug('save_settings) currentIndex={}'.format(_curr_pkg))
+        instance_settings.set_value(self._SETTING_LASTPKG, _curr_pkg)
+        instance_settings.set_value(self._SETTING_LASTLAUNCHFILE, self._widget.launchfile_select.currentText())
+        instance_settings.set_value(self._SETTING_LASTLAUNCHARGS, self._widget.args_input.text())
 
     def restore_settings(self, plugin_settings, instance_settings):
         self._widget.editor = instance_settings.value('editor', 'gedit')
+        self._widget.args_input.setText(instance_settings.value(self._SETTING_LASTLAUNCHARGS, ''))
+        pkg_idx = self._widget.package_select.findText(instance_settings.value(self._SETTING_LASTPKG))
+        if pkg_idx >= 0:
+            self._widget.package_select.blockSignals(True)
+            self._widget.package_select.setCurrentIndex(pkg_idx)
+            self._widget.package_select.blockSignals(False)
+            self._widget.update_launchfiles(pkg_idx)
+            # only set launch file if pkg was restored
+            launch_idx = self._widget.launchfile_select.findText(instance_settings.value(self._SETTING_LASTLAUNCHFILE))
+            if launch_idx >= 0:
+                self._widget.launchfile_select.blockSignals(True)
+                self._widget.launchfile_select.setCurrentIndex(launch_idx)
+                self._widget.launchfile_select.blockSignals(False)
+        self._widget.block_load(False)
+        self._widget.load_launchfile()
 
     def trigger_configuration(self):
         (text, ok) = QInputDialog.getText(self._widget,
